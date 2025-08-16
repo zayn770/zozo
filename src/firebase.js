3e0b3612d2d6e3c0f48c88761f5883565fb76051
zayn770 <point2break@gmail.com>
2025-08-16 16:55:31 +0300

    .env update

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA8bDdbdW9-IP1MKQTOctmc9QGnlo6CPv4",
-  authDomain: "zozo-b47d3.firebaseapp.com",
-  projectId: "zozo-b47d3",
-  storageBucket: "zozo-b47d3.firebasestorage.app",
-  messagingSenderId: "494806756624",
-  appId: "1:494806756624:web:3f8fa66cbf95c13b9ec995",
-  measurementId: "G-9CS692T9J4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyA8bDdbdW9-IP1MKQTOctmc9QGnlo6CPv4",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "zozo-b47d3.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "zozo-b47d3",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "zozo-b47d3.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "494806756624",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:494806756624:web:3f8fa66cbf95c13b9ec995",
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-9CS692T9J4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
